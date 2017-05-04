a7cfd5b42d3b9eae951376e9fe1b99a1b543c245
Jennifer Marsman <jennifermarsman@users.noreply.github.com>
2017-05-04 11:46:51 -0400

    Azure Search integration with bot (#42)
    * Removed old template bot code
    
    * Initial bot template code
    
    * Added Scorable Groups and LUIS integration to the bot
    
    * Fixed spelling mistake in template
    
    * Removed old template bot code
    
    * Initial bot template code
    
    * Added Scorable Groups and LUIS integration to the bot
    
    * Fixed spelling mistake in template
    
    * Azure Search integration with bot.  Still some TODOs but simple case works

---
+++ PictureBot/Dialogs/RootDialog.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Scorables;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+
+namespace PictureBot.Dialogs
+{
+    [LuisModel("TODO-modelID", "TODO-luisKey")]
+    [Serializable]
+    public class RootDialog : DispatchDialog
+    {
+        //private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        //{
+        //    var activity = await result as Activity;
+
+        //    // calculate something for us to return
+        //    int length = (activity.Text ?? string.Empty).Length;
+
+        //    // return our reply to the user
+        //    await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+        //    context.Wait(MessageReceivedAsync);
+        //}
+
+        [RegexPattern("^hello")]
+        [RegexPattern("^hi")]
+        [ScorableGroup(0)]
+        public async Task Hello(IDialogContext context, IActivity activity)
+        {
+            await context.PostAsync("Hello from RegEx!  I am a Photo Organization Bot.  You can ask me things like 'find pictures of food'.  I can also share your photos on Twitter.");
+        }
+
+        [RegexPattern("^help")]
+        [ScorableGroup(0)]
+        public async Task Help(IDialogContext context, IActivity activity)
+        {
+            // TODO: launch help dialog with button menu.  
+            await context.PostAsync("TODO: help info");
+        }
+
+        [LuisIntent("None")]
+        [ScorableGroup(1)]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            // Luis returned with "None" as the winning intent,
+            // so drop down to next level of ScorableGroups.  
+            ContinueWithNextGroup();
+        }
+
+        [LuisIntent("SearchPics")]
+        [ScorableGroup(1)]
+        public async Task SearchPics(IDialogContext context, LuisResult result)
+        {
+            // TODO: change LUIS to only have facet entity?  
+            // TODO: LUIS deserialization errors with entities - fixed?  
+            string gender = null;
+            string age = null;
+            string emotion = null;
+            string facet = null;
+
+            EntityRecommendation rec;
+            if (result.TryFindEntity("gender", out rec)) gender = rec.Entity;
+            if (result.TryFindEntity("age", out rec)) age = rec.Entity;
+            if (result.TryFindEntity("emotion", out rec)) emotion = rec.Entity;
+            if (result.TryFindEntity("facet", out rec)) facet = rec.Entity;
+
+            await context.PostAsync($"Searching pictures...");
+            context.Call(new SearchDialog(facet), ResumeAfterSearchDialog);
+        }
+
+        private async Task ResumeAfterSearchDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            // TODO: remove if this isn't used
+            await context.PostAsync("Done searching pictures");
+        }
+
+        [LuisIntent("SharePic")]
+        [ScorableGroup(1)]
+        public async Task SharePic(IDialogContext context, LuisResult result)
+        {
+            PromptDialog.Confirm(context, AfterShareAsync,
+                "Are you sure you want to tweet this picture?");            
+        }
+
+        private async Task AfterShareAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            if (result.GetAwaiter().GetResult() == true)
+            {
+                // Yes, share the picture.
+                // TODO: change this code to post a tweet
+                await context.PostAsync("Posting tweet...");
+            }
+            else
+            {
+                // No, don't share the picture.  
+                await context.PostAsync("OK, I won't share it.");
+            }
+        }
+
+        [LuisIntent("Greeting")]
+        [ScorableGroup(1)]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            // TODO: remove this?
+            // Duplicate logic, for a teachable moment.  Try commenting out the "hello" RegEx above!  
+            await context.PostAsync("Hello from LUIS!  I am a Photo Organization Bot.  You can ask me things like 'find pictures of food'.  I can also share your photos on Twitter.");
+        }
+
+        // Since none of the scorables in previous group won, the dialog sends a help message.
+        [MethodBind]
+        [ScorableGroup(2)]
+        public async Task Default(IDialogContext context, IActivity activity)
+        {
+            await context.PostAsync("I'm sorry. I didn't understand you.");
+            await context.PostAsync("You can tell me to find photos and tweet them.  Here is an example: \"find pictures of food\"");
+        }
+
+    }
+}
---
+++ PictureBot/Models/SearchHit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PictureBot.Models
+{
+    [Serializable]
+    public class SearchHit
+    {
+        public SearchHit()
+        {
+            this.PropertyBag = new Dictionary<string, object>();
+        }
+
+        public string Key { get; set; }
+
+        public string Title { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public IDictionary<string, object> PropertyBag { get; set; }
+    }
+}
