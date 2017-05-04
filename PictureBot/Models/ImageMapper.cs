e0b0216017fb3f5d13a86997c98717ef71ed695f
Jennifer Marsman <jennmar@microsoft.com>
2017-05-04 03:34:01 -0400

    Azure Search integration with bot.  Still some TODOs but simple case works

---
+++ PictureBot/Models/ImageMapper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Search.Models;
+
+namespace PictureBot.Models
+{
+    public class ImageMapper 
+    {
+        public static SearchHit ToSearchHit(SearchResult hit)
+        {
+            var searchHit = new SearchHit
+            {
+                Key = (string)hit.Document["rid"],
+                Title = (string)hit.Document["FileName"],
+                PictureUrl = (string)hit.Document["BlobUri"],
+                Description = (string)hit.Document["Caption"]
+            };
+
+            object Tags;
+            if (hit.Document.TryGetValue("Tags", out Tags))
+            {
+                searchHit.PropertyBag.Add("Tags", Tags);
+            }
+
+            object NumFaces;
+            if (hit.Document.TryGetValue("NumFaces", out NumFaces))
+            {
+                searchHit.PropertyBag.Add("NumFaces", NumFaces);
+            }
+
+            object Faces;
+            if (hit.Document.TryGetValue("Faces", out Faces))
+            {
+                searchHit.PropertyBag.Add("Faces", Faces);
+            }
+
+            return searchHit;
+        }
+
+    }
+}
